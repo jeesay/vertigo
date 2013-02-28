3e8ca41e670b43fc6fcbdf08ccd741fea7f55e8f
CoiLatO <olivier.cato@orange.fr>
2013-02-28 14:26:03 +0100

    test 2

--- scenegraph/Material.java
+++ scenegraph/Material.java
@@ -20,9 +20,9 @@
  * 02110-1301  USA, or see the FSF site: http://www.fsf.org.
  *
  * Authors :
- *Florin Buga
- *Olivier Catoliquot
- *Clement Delestre
+ * Florin Buga
+ * Olivier Catoliquot
+ * Clement Delestre
  */
 
 class Material{
