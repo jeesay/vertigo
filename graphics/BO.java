f7a052063ab1a8c6ab2aeeb8bcf6986923c4f95e
jeesay <jjeesay@gmail.com>
2013-03-08 10:14:38 +0100

    Update BO.java
    first commit

---
+++ graphics/BO.java
@@ -0,0 +1,19 @@
+ 
+ public class BO {
+   
+   private int handle;
+   
+   public BO() {
+     handle = -1;
+   }
+   
+   public void setHandle(int value) {
+        handle = value;
+    }
+
+
+    public int getHandle() {
+        return handle;
+    }
+
+} // End of class BO
