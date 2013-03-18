cdc6ed7c26e65c9b88e15568fd83f60ca24d5dde
CoiLatO <olivier.cato@orange.fr>
2013-03-18 15:46:28 +0100

    update bo

---
+++ graphics/Renderer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vertigo.graphics;
+
+/**
+ *
+ * @author tomo
+ */
+public interface Renderer {
+   
+    public void display();
+    
+}
