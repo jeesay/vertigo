3e1fc22a8a88bb6f742f4284cb3fb53f42515914
CoiLatO <olivier.cato@orange.fr>
2013-03-18 15:46:35 +0100

    update bo

---
+++ graphics/VBO.java
@@ -0,0 +1,20 @@
+package vertigo.graphics;
+
+public class VBO extends BO {
+  
+  private FloatBuffer buffer;
+  
+  public VBO() {
+    super();
+  }
+  
+  public void setFloatBuffer(String type, FloatBuffer buf) {
+        this.type = type;
+        buffer = buf;
+    }
+
+
+    public FloatBuffer getFloatBuffer() {
+        return buffer;
+    }
+} // End of class VBO
