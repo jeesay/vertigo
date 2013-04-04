a0bd2302634cea6c72355a74fe9de200db46e45c
Clem-D <c.delestre@caramail.fr>
2013-04-04 16:55:47 +0200

    fix bugs

--- scenegraph/Node.java
+++ scenegraph/Node.java
@@ -1,7 +1,7 @@
 /*
  * $Id:$
  *
- * crazybio_viewer: 3D Viewer Plugin for ImageJ.
+ * Vertigo: 3D Viewer Plugin for ImageJ.
  * Copyright (C) 2013  Jean-Christophe Taveau.
  *
  * This library is free software; you can redistribute it and/or
@@ -32,30 +32,30 @@ import vertigo.math.Matrix4;
 import vertigo.math.Vector3;
 import java.util.Iterator;
 
-    /**
-     * Class Node
-     * @author Florin Buga
-     * @author Olivier Catoliquot
-     * @author Clement Delestre
-     * @version 0.1
-     *
-     */
+/**
+ * Class Node
+ *
+ * @author Florin Buga
+ * @author Olivier Catoliquot
+ * @author Clement Delestre
+ * @version 0.1
+ *
+ */
 public class Node {
 
     private Node parent;
     private ArrayList<Node> children;
     private Matrix4 matrix;
-    private Matrix4 ModelView;
-    private AABB bbox;
+    private Matrix4 modelMatrix;
+    protected AABB bbox;
     private byte dirty_;
+    protected boolean drawable_;
     protected String name;
-
-
-    public static int MATRIX = 0x1;
-    public static int GEOMETRY = 0x2;
-    public static int SHADER = 0x4;
-    public static int AABB = 0x8;
-    public static int COLOR = 0xA;
+    public static byte MATRIX = 0x1;
+    public static byte AABB = 0x2;
+    public static byte VBO = 0x4;
+    public static byte SHADER = 0x8;
+    public static byte COLOR = 0xA;
 
     /**
      * Constructor
@@ -65,14 +65,16 @@ public class Node {
         Node parent = null;
         matrix = new Matrix4();
         matrix.setIdentity();
-        //bbox = new AABB();
+        bbox = new AABB();
         name = "node";
-        //dirty_ = 0xff;
+        dirty_ = (byte) 0xff;
+        drawable_ = false;
         //need to comments for compile. Need to compile for tests.
     }
 
     /**
      * Set Node name
+     *
      * @param String
      */
     public void setName(String name) {
@@ -81,6 +83,7 @@ public class Node {
 
     /**
      * Get Node name
+     *
      * @return String
      */
     public String getName() {
@@ -89,6 +92,7 @@ public class Node {
 
     /**
      * Set node's Parent
+     *
      * @param Node
      */
     public void setParent(Node a_node) {
@@ -97,6 +101,7 @@ public class Node {
 
     /**
      * Method for add one child.
+     *
      * @param Node anode
      */
     public void add(Node a_node) {
@@ -107,12 +112,12 @@ public class Node {
     /**
      * Add a new node of type 'type'
      *
-     * @param String type of node added to the scene graph.
-     * Allowed types are: Branch, Camera, Cube, Light, Node, Shape, Transform
+     * @param String type of node added to the scene graph. Allowed types are:
+     * Branch, Camera, Cube, Light, Node, Shape, Transform
      * @return a node of g‪ivent type.
      */
-    public Node addNewNode(String type){
-        Node newNode= NodeFactory.get(type);
+    public Node addNewNode(String type) {
+        Node newNode = NodeFactory.get(type);
         this.add(newNode);
         newNode.setParent(this);
         return newNode;
@@ -120,6 +125,7 @@ public class Node {
 
     /**
      * Remove one child.
+     *
      * @param Node anode
      */
     public void remove(Node a_node) {
@@ -128,6 +134,7 @@ public class Node {
 
     /**
      * Get the Node's children.
+     *
      * @param Node anode
      * @return ArrayList
      */
@@ -168,24 +175,27 @@ public class Node {
      *
      * @return Node.
      */
-    /*
     public AABB getBoundingBox() {
-        if (bbox.isEmpty() ) {
-            for (Node child : getChildren() )
-                bbox.expand(child.getBoundingBox() );
-            setDirty(AABB,false);
+        if (bbox.isEmpty() || isDirty(Node.AABB)) {
+            updateBoundingBox();
         }
-
         return bbox;
     }
-*/
+
+    public void updateBoundingBox() {
+        for (Node child : getChildren()) {
+            bbox.expand(child.getBoundingBox());
+        }
+        setDirty(AABB, false);
+    }
+
     /**
      * Set Translation
      *
      * @param tx,ty,tz as floats
      */
-    public void setTranslation(float tx, float ty, float tz){
-        matrix.setTranslation(new Vector3(tx,ty,tz));
+    public void setTranslation(float tx, float ty, float tz) {
+        matrix.setTranslation(new Vector3(tx, ty, tz));
     }
 
     /**
@@ -193,8 +203,8 @@ public class Node {
      *
      * @param tx,ty,tz as floats
      */
-    public void setPosition(float tx, float ty, float tz){
-        setTranslation(tx,ty,tz);
+    public void setPosition(float tx, float ty, float tz) {
+        setTranslation(tx, ty, tz);
     }
 
     /**
@@ -202,7 +212,7 @@ public class Node {
      *
      * @param x,y,z as floats
      */
-    public void setDirection(float x,float y, float z){
+    public void setDirection(float x, float y, float z) {
         //TODO with Matrix4 (look at)
     }
 
@@ -211,18 +221,44 @@ public class Node {
      *
      * @param scale as floats
      */
-    public void setScale(float s){
+    public void setScale(float s) {
         //TODO
         matrix.setScale(s);
     }
-   
+
+    /**
+     * Get the this matrix
+     *
+     * @return Node.
+     */
+    public Matrix4 getMatrix() {
+        return matrix;
+    }
+
+    /**
+     * Get the model matrix from local to world coordinates
+     *
+     * @return Node.
+     */
+    public Matrix4 getModelMatrix() {
+        return modelMatrix;
+    }
+
+    /**
+     * Set the model matrix expressed in world coordinates
+     *
+     */
+    public void setModelMatrix(Matrix4 m) {
+        modelMatrix = m;
+    }
+
     /**
      * Get status of this node
      *
      * @return Flag giving the overall status of this node
      */
-    public boolean isDirty(){
-        return ( (dirty_ & 0xff) == 0xff);
+    public boolean isDirty() {
+        return ((dirty_ & 0xff) == 0xff);
     }
 
     /**
@@ -231,8 +267,8 @@ public class Node {
      * @param Only check the status of a given properties: MATRIX, VBO, SHADER
      * @return Flag giving the status of this node
      */
-    public boolean isDirty(int flag){
-        return ( (dirty_ & flag) == flag);
+    public boolean isDirty(int flag) {
+        return ((dirty_ & flag) == flag);
     }
 
     /**
@@ -241,14 +277,14 @@ public class Node {
      * @param Only check the status of a given properties: MATRIX, VBO, SHADER
      * @return Flag giving the status of this node
      */
-    /*
-    public boolean setDirty(int flag, boolean value){
-        if (value)
-            dirty_ = dirty_ | flag;
-        else
-            dirty_ = dirty_ & ~flag;
+    public void setDirty(byte flag, boolean value) {
+        if (value) {
+            dirty_ = (byte) (dirty_ | flag);
+        } else {
+            dirty_ = (byte) (dirty_ & ~flag);
+        }
     }
-*/
+
     /**
      * Set status of this node and children
      *
@@ -256,38 +292,37 @@ public class Node {
      * @return Flag giving the status of this node
      */
     /*
-    public boolean setAllDirty(int flag, boolean value){
-        if (value)
-            dirty_ = dirty_ | flag;
-        else
-            dirty_ = dirty_ & ~flag;
-        for (Node n : children)
-            n.setAllDirty(flag,value);
-    }
-*/
-
-    public boolean check(){
+     public boolean setAllDirty(int flag, boolean value){
+     if (value)
+     dirty_ = dirty_ | flag;
+     else
+     dirty_ = dirty_ & ~flag;
+     for (Node n : children)
+     n.setAllDirty(flag,value);
+     }
+     */
+    public boolean check() {
         return true; // TODO
     }
-/*
-    public void accept(Visitor visitor) {
-        // In node, the visitor must be accepted by children to
-        // propagate its action.
-        visitor.visit(this);
-        for (Node child : getChildren()) {
-            child.accept(visitor);
-        }
-    }
-*/
+    /*
+     public void accept(Visitor visitor) {
+     // In node, the visitor must be accepted by children to
+     // propagate its action.
+     visitor.visit(this);
+     for (Node child : getChildren()) {
+     child.accept(visitor);
+     }
+     }
+     */
 
-   /**
+    /**
      * Get the root.
      *
      * @return Node.
      */
     public Node traverseUp() {
         if (parent == null) {
-            System.out.println("La racine a été trouvée ! Elle se nomme "+this.getName());
+            System.out.println("La racine a été trouvée ! Elle se nomme " + this.getName());
             return this;
         } else {
             Node a_node = this.getParent();
@@ -295,43 +330,48 @@ public class Node {
         }
         return null;
     }
-/**
+
+    /**
      * Get the leafs.
      *
      * @return Node.
      */
     public void traverseDown() {
         if (children.isEmpty()) {
-            System.out.println("Une feuille a été trouvée ! Elle se nomme "+this.getName());
-            
+            System.out.println("Une feuille a été trouvée ! Elle se nomme " + this.getName());
+
         } else {
             for (Iterator<Node> it = children.iterator(); it.hasNext();) {
                 Node nodetemp = it.next();
                 nodetemp.traverseDown();
             }
         }
-        
+
     }
-   public void traverseDownT(){
-       this.traverseDownT(this,0);
-     
-   }
-   public void traverseDownT(Node a_node, int loop){
-       String arrow="";
-       for (int i=0;i<loop;i++){
-           arrow+="-";
-       }
-       arrow+="->";
-       System.out.println(arrow+" "+a_node.getName());
-       if (!a_node.children.isEmpty()){
+
+    public void traverseDownT() {
+        this.traverseDownT(this, 0);
+
+    }
+
+    public void traverseDownT(Node a_node, int loop) {
+        String arrow = "";
+        for (int i = 0; i < loop; i++) {
+            arrow += "-";
+        }
+        arrow += "->";
+        System.out.println(arrow + " " + a_node.getName());
+        if (!a_node.children.isEmpty()) {
             loop++;
-             for (Iterator<Node> it = children.iterator(); it.hasNext();) {
+            for (Iterator<Node> it = children.iterator(); it.hasNext();) {
                 Node nodetemp = it.next();
-                nodetemp.traverseDownT(nodetemp,loop);
+                nodetemp.traverseDownT(nodetemp, loop);
             }
-           
-       }
-   }
 
+        }
+    }
 
+    public boolean isDrawable() {
+        return drawable_;
+    }
 } // End of class Node
