45f028914d9f62256095f23d2eb7815974ce87ea
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2013-04-08 15:49:17 +0200

    first commit

---
+++ test/test0.js
@@ -0,0 +1,18 @@
+// Test0: Open an OpenGL window
+// Vertigo project
+// April 2013
+
+// Creation and init viewer
+var viewer = newInstance("Vertigo_Viewer");
+viewer.setTitle("Test #0");
+viewer.setBackgroundColor(255,128,50); // Orange
+viewer.setDimension(640,480);
+
+// Render
+viewer.show();
+
+// F U N C T I O N S
+// Load and instanciation of a class
+function newInstance(classname) {
+  return IJ.getClassLoader().loadClass(classname).newInstance();
+}
