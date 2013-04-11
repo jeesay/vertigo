8639b6ce9a6fb1827f744c1f0b9100b437aad11a
Clem-D <c.delestre@caramail.fr>
2013-04-11 12:46:51 +0200

    Add new Shapes and Nodes.

---
+++ scenegraph/Group.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vertigo.scenegraph;
+
+import vertigo.graphics.Visitor;
+
+/**
+ *
+ * @author Clement DELESTRE
+ */
+public class Group extends Node {
+
+    @Override
+    public void accept(Visitor visitor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ scenegraph/shapes/Pyramid.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vertigo.scenegraph.shapes;
+
+import vertigo.scenegraph.Shape;
+
+/**
+ *
+ * @author Clement DELESTRE
+ */
+public class Pyramid extends Shape{
+    
+}
