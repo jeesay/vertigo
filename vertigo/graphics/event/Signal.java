6cdabda7111af10b3a63d5e91b690b01639ad3aa
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2013-04-22 17:15:22 +0200

    update

--- vertigo/graphics/event/Signal.java
+++ vertigo/graphics/event/Signal.java
@@ -32,14 +32,19 @@ package vertigo.graphics.event;
  */
 public class Signal {
 
+    public static final int NO_BUTTON = 0;
     public static final int BUTTON_LEFT = 1;
     public static final int BUTTON_MIDDLE = 2;
-    public static final int BUTTON_RIGHT = 4;
+    public static final int BUTTON_RIGHT = 3;
     public static final int WHEEL_UP = 1;
     public static final int WHEEL_DOWN = -1;
-    public static final int PUSH = 2;
-    public static final int RELEASED = 3;
-    public static final int CLICK = 1; 
+    public static final int PRESSED = 101;
+    public static final int RELEASED = 102;
+    public static final int CLICKED = 103; 
+    public static final int MOVED = 104; 
+    public static final int DRAGGED = 105; 
+    public static final int ENTERED = 106; 
+    public static final int EXITED = 107; 
     
     
    
