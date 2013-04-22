425121e1c545067d4a4e70c43cd5032617369cf7
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2013-04-22 17:13:45 +0200

    first commit

---
+++ vertigo/graphics/event/ViewportSignal.java
@@ -0,0 +1,71 @@
+/*
+ * $Id:$
+ *
+ * crazybio_viewer: 3D Viewer Plugin for ImageJ.
+ * Copyright (C) 2013  Jean-Christophe Taveau.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA, or see the FSF site: http://www.fsf.org.
+ *
+ * Authors :
+ *Florin Buga
+ *Olivier Catoliquot
+ *Clement Delestre
+ */
+package vertigo.graphics.event;
+
+/**
+ * @author : Jean-Christophe Taveau
+ */
+public class ViewportSignal extends Signal {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public ViewportSignal() {
+    
+    }
+ 
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+ 
+    public void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void set(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    
+} // end of class ViewportSignal
