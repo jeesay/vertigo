3647d03a3c28341b81da7685e9f3c0a6da84601d
jeesay <jjeesay@gmail.com>
2013-05-06 16:52:15 +0300

    Update cube.js

--- test/cube.js
+++ test/cube.js
@@ -1,6 +1,11 @@
 var viewer = newInstance("Vertigo_Viewer");
 viewer.setTitle("Cube");
 viewer.setBackgroundColor(50,200,0);
+
+// Get Camera and translate it 
+var cam = viewer.getCamera();
+cam.setPosition(0.0,0.0,8.0);
+
 // Get Scene 
 var scene = viewer.getScene();
 scene.addNewNode("WireCube");
