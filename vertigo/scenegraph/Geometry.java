fd6be4e844e8973938dc38f7a673222779c5d460
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2013-05-17 11:13:24 +0200

    update

--- vertigo/scenegraph/Geometry.java
+++ vertigo/scenegraph/Geometry.java
@@ -46,31 +46,34 @@ public class Geometry {
  * A geometry is composed of several BO.
  * @see Geometry#addBuffer(java.lang.String, float[]) 
  */
-    private ArrayList<BO> buffers;
+    private ArrayList<BO> boList;
     /**
- * The data of each BO.
- * @see Geometry#getBufferData() 
- */
-    private BufferData buffdata;
+     * The data of each BO.
+     * @see Geometry#getBufferData() 
+     */
+    private ArrayList<BufferData> buffList;
 
     public Geometry() {
-        buffers = new ArrayList<BO>();
+        boList = new ArrayList<BO>();
+        buffList = new ArrayList<BufferData>();
     }
 
-    public BufferData getBufferData() {
-        return buffdata;
+    public ArrayList<BufferData> getBufferData() {
+        return buffList;
     }
 
-    public ArrayList<BO> getBuffers() {
-        return buffers;
+    public ArrayList<BO> getAllBO() {
+        return boList;
     }
 
-    public int getCount() {
-        return buffdata.getCapacity();
+    public int getBufferCount() {
+        return buffList.size();
     }
 
+/***
+OBSOLETE
     public void addBuffert(String[] types, float[] data) {
-        buffdata = new BufferData(data);
+        BufferData buff = new BufferData(data);
         int stride = 0;
         int offset = 0;
         for (int i = 0; i < types.length; i++) {
@@ -78,27 +81,19 @@ public class Geometry {
         }
         for (int i = 0; i < types.length; i++) {
             VBO vbo = new VBO(stride, offset, types[i]);
-            vbo.setBuffData(buffdata);
-            buffers.add(vbo);
+            vbo.setBuffData(buff);
+            boList.add(vbo);
             offset += getSize(types[i]);
         }
     }
+***/
 
     /**
      * Gets the number of BO (VBO & IBO)
      * @return number of BO
      */
     public int getNumberBO() {
-        return buffers.size();
-    }
-
-    private int getSize(String type) {
-        if (type.contains("3")) {
-            return 3;
-        } else if (type.contains("2")) {
-            return 2;
-        }
-        return 4;
+        return boList.size();
     }
 
     /**
@@ -109,12 +104,11 @@ public class Geometry {
      * (coordinates, normal, colors,etc.).
      */
     public void addBuffer(String type, float[] data) {
-
-        VBO vbo = new VBO(type);
-        buffdata = new BufferData(data);
-        vbo.setBuffData(buffdata);
-        buffers.add(vbo);
-
+        VBO vbo = new VBO(type,0,getSize(type) );
+        BufferData buf = new BufferData(data);
+        vbo.setBufferData(buf);
+        boList.add(vbo);
+        buffList.add(buf);
     }
 
     /**
@@ -122,20 +116,30 @@ public class Geometry {
      * May contain various types of data: XYZ-coordinates, Normals, Colors,
      * UV-TexCoords,etc.
      *
-     * @param types - Data type: VEC3F,COL3F,TEX2F,TEX3F, NORM3F,etc.
+     * @param types - Data type: V3F,C3F,T2F,T3F, N3F,etc.
      * @param primitives - an array containing the data for each vertex
      * (coordinates, normal, colors,etc.).
      * @param contents - a String containing the various data type present in
-     * the primitives array. Ex: "[VEC3F,COL3F,NORM3F,TEX2F]" corresponds to
+     * the primitives array. Ex: "[V3F,C3F,N3F,T2F]" corresponds to
      * Vertex, Color, Normal and finally TexCoords data for each vertex.
      */
     public void addBuffer(String[] types, float[] data) {
-        FloatBuffer buf = BufferTools.newFloatBuffer(data.length);
-        buf.put(data);
-        buf.rewind();
-        VBO vbo = new VBO();
-        vbo.setFloatBuffer(types, buf);
-        buffers.add(vbo);
+        BufferData buf = new BufferData(data);
+        buffList.add(buf);
+
+        // Calc stride
+        int stride = 0;
+        for (int i = 0; i < types.length; i++) {
+            stride += getSize(types[i]);
+        }
+        int offset = 0;
+        for (int i = 0; i < types.length; i++) {
+            VBO vbo = new VBO(types[i], offset, stride );
+            vbo.setBufferData(buf);
+            boList.add(vbo);
+            offset += getSize(types[i]);
+        }
+
     }
 
     /**
@@ -159,7 +163,7 @@ public class Geometry {
         buf.rewind();
         IBO ibo = new IBO();
         ibo.setIntBuffer(buf);
-        buffers.add(ibo);
+        boList.add(ibo);
     }
 
     /**
@@ -187,8 +191,19 @@ public class Geometry {
     }
 
     public BO getBO(int index) {
-        // HACK: if (buffers.isEmpty() ) {System.out.println("Geom. buffer is empty");return null;}
-        // HACK: for (BO bo : buffers) System.out.println("Geom. buffer "+bo);
-        return buffers.get(index);
+        // HACK: if (boList.isEmpty() ) {System.out.println("Geom. buffer is empty");return null;}
+        // HACK: for (BO bo : boList) System.out.println("Geom. buffer "+bo);
+        return boList.get(index);
     }
+
+    private int getSize(String type) {
+        if (type.contains("3")) {
+            return 3;
+        } else if (type.contains("2")) {
+            return 2;
+        }
+        return 4;
+    }
+
+
 } // end of class Geometry
